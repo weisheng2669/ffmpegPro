f84513ead6bdb5808953ab096e99ff8c585ce164
wang chao <2669148698@qq.com>
2019-11-21 15:21:44 +0800

    视频中操作

--- ffmpegPro/ffmpegPro.cpp
+++ ffmpegPro/ffmpegPro.cpp
@@ -9,21 +9,6 @@ using namespace std;
 
 int main()
 { 
-	//转封装
-	/*
-	transmuxer t;
-	t.transport_file("D:\\audioAndvideo\\transmuxer\\live.mp4", 
-		"D:\\audioAndvideo\\transmuxer\\live.avi");
-    */
-	//解封装
-	/*
-	demuxer_relation d;
-	d.demuxer("D:\\audioAndvideo\\demuxer\\test.mp4");
-	*/
-	//封装
-	muxer m;
-	m.muxer_file("D:\\audioAndvideo\\muxer\\test_de.h264","D:\\audioAndvideo\\muxer\\test_de.mp3",
-		"D:\\audioAndvideo\\muxer\\test_de.mp4");
 
 	return 0;
 }
